d0a986f4e2e2b820be666b9dbac90002e1e0f4c3
ap-konovalov <sanches-zi@mail.ru>
2019-04-28 15:40:02 +0300

    Создали космический фон

---
+++ SpaceEnslavers/Space.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEnslavers
+{
+    class Space : BaseObject
+    {
+        Image background = Image.FromFile(@"..\..\background.jpeg");
+
+        public Space(Point position, Point dir, Size size) : base(position, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+          Game.Buffer.Graphics.DrawImage(background, Position.X, Position.Y,800,600);
+        }
+    }
+}
